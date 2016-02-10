7c7f71529a2f0eeaac503ebc05eb2531b0483d5c
‘Jerry <jerryandhisemail@gmail.com>
2016-02-10 11:38:27 -0800

    Changed the layout of the array to make more sense

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 // Making a function to ask questions more easily
 function ask(question) {
   this.question = question;
-  var answer = prompt(question[0]).toUpperCase();
+  var answer = prompt(question[1]).toUpperCase();
   if (answer === question[2]) {
-      console.log(this.question[1] + ": " + "Answer: " + answer);
+      console.log(this.question[0] + ": " + "Answer: " + answer);
   } else if (answer === question[3]) {
-      console.log(this.question[1] + ": " + "Answer: " + answer);
+      console.log(this.question[0] + ": " + "Answer: " + answer);
   } else {
       alert("Please answer in Y/N format.");
       console.log("Invalid response - Asking the question again");
@@ -15,12 +15,12 @@ function ask(question) {
   }
 }
 
-// Arrays are in this format ["Question", "Name of Question", "Correct answer", "Wrong answer"]
-var question1 = ["Does Jerry want to be in a International Hotdog Eating contest?" , "Question 1", "N", "Y"];
-var question2 = ["Is 'Kanye West' one of Jerry\'s prior nicknames?" , "Question 2", "N", "Y"];
-var question3 = ["Has Jerry met Dumbledore?" , "Question 3", "Y", "N"];
-var question4 = ["Is Aristotle Jerry's father?" , "Question 4", "Y", "N"];
-var question5 = ["Did Jerry write the description for '10 Things I Hate About You' on Netflix?" , "Question 5", "Y", "N"];
+// Arrays are in this format ["Name of Question", "Question", "Correct answer", "Wrong answer"]
+var question1 = ["Question 1", "Does Jerry want to be in a International Hotdog Eating contest?", "N", "Y"];
+var question2 = ["Question 2", "Is 'Kanye West' one of Jerry\'s prior nicknames?", "N", "Y"];
+var question3 = ["Question 3", "Has Jerry met Dumbledore?", "Y", "N"];
+var question4 = ["Question 4", "Is Aristotle Jerry's father?", "Y", "N"];
+var question5 = ["Question 5", "Did Jerry write the description for '10 Things I Hate About You' on Netflix?", "Y", "N"];
 
 ask(question1);
 ask(question2);
