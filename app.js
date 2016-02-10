bc510755b15efc058779b0b3c91beb929f0c0f43
‘Jerry <jerryandhisemail@gmail.com>
2016-02-10 09:54:20 -0800

    Added questions into app.js

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-// Making a guessing game with 5 questions with different responses if they answer it correctly or not
+// Prompt the use with 5 trivia questions in Y/N format
 
-// Assigning the questions
-var q1 = "What's up?";
-var q2 = "Not much?";
-var q3 = "Cool. You're cool?";
-var q4 = "Thanks?";
-
-/* var questionsArray = [q1, q2, q3, q4];
-var questionCount = function(var i = 0; i < questionsArray.length; i++) {
-  return questionsArray[i];
-}; */
-
-// Making a function to ask the questions more easily
+// Making a function to ask questions more easily
 function ask(question) {
   var answer = prompt(question);
   console.log("The user's answer is " + answer);
 }
 
+// Assigning the questions
+var q1 = "Does Jerry want to be in a International Hotdog Eating contest?";
+var q2 = "Is "Kanye West" one of Jerry\'s prior nicknames?";
+var q3 = "Has Jerry met Dumbledore?";
+var q4 = "Is Aristotle Jerry's father?";
+var q5 = "Did Jerry write the description for '10 Things I Hate About You' on Netflix?";
+
 ask(q1);
 ask(q2);
 ask(q3);
 ask(q4);
-
-q1;
+ask(q5);
