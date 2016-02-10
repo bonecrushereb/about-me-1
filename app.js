ae246e03628455dbe0e12dd32302a72deba47f6e
‘Jerry <jerryandhisemail@gmail.com>
2016-02-10 10:09:08 -0800

    functional app.js changes for accepting y/n responses

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 
 // Making a function to ask questions more easily
 function ask(question) {
-  var answer = prompt(question);
-  console.log("The user's answer is " + answer);
+  this.question = question;
+  var answer = prompt(question).toUpperCase();
+  if (answer === "Y" || answer === "N") {
+      console.log("The user's answer is " + answer);
+  } else {
+      alert("Please answer in Y/N format.");
+      console.log("Invalid response - Asking the question again");
+      ask(this.question);
+  }
 }
 
 // Assigning the questions
 var q1 = "Does Jerry want to be in a International Hotdog Eating contest?";
-var q2 = "Is "Kanye West" one of Jerry\'s prior nicknames?";
+var q2 = "Is 'Kanye West' one of Jerry\'s prior nicknames?";
 var q3 = "Has Jerry met Dumbledore?";
 var q4 = "Is Aristotle Jerry's father?";
 var q5 = "Did Jerry write the description for '10 Things I Hate About You' on Netflix?";
